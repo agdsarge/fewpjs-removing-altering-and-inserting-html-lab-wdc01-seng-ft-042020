c4c482e660cb589016fdc86fa6e7c845f6c0f1ce
agdsarge <agdsargeant@gmail.com>
2020-06-01 05:26:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("#main").remove();
+
+let x = document.createElement("h1");
+x.id = "victory";
+x.innerHTML = "YOUR-NAME is the champion"; // :)
+
+let newHeader = document.body.appendChild(x);
